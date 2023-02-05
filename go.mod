alecthomas/genh

feat: initial commit

go.mod
@@ -1,0 +1,20 @@
+module github.com/alecthomas/genh
+
+go 1.19
+
+require (
+	github.com/alecthomas/kong v0.7.1
+	golang.org/x/tools v0.3.0
+)
+
+require (
+	github.com/alecthomas/repr v0.1.0 // indirect
+	github.com/hexops/gotextdiff v1.0.3 // indirect
+)
+
+require (
+	github.com/alecthomas/assert/v2 v2.2.0
+	github.com/alecthomas/participle/v2 v2.0.0-beta.5
+	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+)

thnxdev/happy

go.mod
@@ -1,0 +1,19 @@
+module github.com/thnxdev/happy
+
+go 1.19
+
+require (
+	github.com/alecthomas/kong v0.7.1
+	golang.org/x/tools v0.3.0
+)
+
+require (
+	github.com/alecthomas/repr v0.1.0 // indirect
+	github.com/hexops/gotextdiff v1.0.3 // indirect
+)
+
+require (
+	github.com/alecthomas/assert/v2 v2.2.0
+	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+)